5847c634c779a503d42fafc90d490628dd7669ed
FgPikaboo <pikaboosimulator@gmail.com>
2026-01-12 13:18:38 +0100

    Premier Changement Final Part 2

--- proc/walls.mjs
+++ proc/walls.mjs
@@ -1,5 +1,11 @@
 import { getScreenWidth, getScreenHeight } from "../engine/terminal-engine.mjs"
 
+/**
+ * Verifie si le Snake a depasser la limite autoriser
+ * @param {number} x La position lateral du Snake
+ * @param {number} y La hauteur du Snake
+ * @returns {string} Renvoie l'instruction detecter
+ */
 export function isWallAt(x, y) {
 	if (x <= 0) {
 		return 'x_wall_left'
