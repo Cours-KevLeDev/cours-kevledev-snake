e5dafd56f86ddbe7d9dd7b865c47ecd8f2d5342d
FgPikaboo <pikaboosimulator@gmail.com>
2026-01-15 12:20:26 +0100

    1er Changement final

--- proc/game.mjs
+++ proc/game.mjs
@@ -1,5 +1,9 @@
 export let game_is_loop
 
+/**
+ * Parametre d'activation si le Snake fait une boucle ou non
+ * @param {boolean} infiniteMove Quand on fait une boucle
+ */
 export function initGame(infiniteMove) {
 	if (infiniteMove) {
 		game_is_loop = true
