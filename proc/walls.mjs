650eb4253447c8f01e1d489d1e188ddb6ddddd77
FgPikaboo <pikaboosimulator@gmail.com>
2026-01-16 19:18:30 +0100

    Bug Fix #1

--- proc/walls.mjs
+++ proc/walls.mjs
@@ -7,7 +7,7 @@ import { getScreenWidth, getScreenHeight } from "../engine/terminal-engine.mjs"
  * @returns {boolean} Renvoie si la collision est detecter ou non
  */
 export function isWallAt(x, y) {
-	if (x < 0 || x > getScreenWidth() || y < 0 || y > getScreenHeight()) {
+	if (x <= 0 || x >= getScreenWidth() || y <= 0 || y >= getScreenHeight()) {
 		return true
 	}
 	return false
