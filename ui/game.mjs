66426d86f0f89e3efad0f802afef17eed9540c45
FgPikaboo <pikaboosimulator@gmail.com>
2026-01-21 19:45:51 +0100

    4.5: Move Cursor

--- ui/game.mjs
+++ ui/game.mjs
@@ -1,4 +1,8 @@
-import { initTerminal2DEngine, initKeyboard, drawString, getScreenHeight, getScreenWidth } from "../engine/terminal-engine.mjs";
+import { initTerminal2DEngine, initKeyboard, drawString, getScreenHeight, getScreenWidth, onkey, clear } from "../engine/terminal-engine.mjs";
+
+function testError() {
+	console.error(new Date().toISOString(),position_cursor.y)
+}
 
 const MENU_OPTION = {
 	M_Normal: 'Mode: Normal',
@@ -10,12 +14,62 @@ const position_option = {
 	y: Math.ceil(getScreenHeight()/2)
 }
 
+const position_cursor = {
+	x: position_option.x - 2,
+	y: position_option.y
+}
+
+function isWallAtCursor(y) {
+	if (y !== position_option.y && y !== position_option.y+1) {
+		return true
+	}
+	return false
+}
+
+function moveDown() {
+	const isWallNext = isWallAtCursor(position_cursor.y + 1)
+	if (isWallNext) {
+		return
+	}
+	position_cursor.y += 1
+	testError()
+}
+function moveUp() {
+	const isWallNext = isWallAtCursor(position_cursor.y - 1)
+	if (isWallNext) {
+		return
+	}
+	position_cursor.y -= 1
+	testError()
+}
+
+const KEY_NAME = {
+	DOWN: 'down',
+	UP: 'up'
+}
+function applyDirection(key) {
+	clear()
+	switch (key.name) {
+		case KEY_NAME.DOWN:
+			moveDown()
+			break
+		case KEY_NAME.UP:
+			moveUp()
+			break
+	}
+	drawMenuMode()
+}
+
 function drawMenuMode() {
-	drawString(position_option.x-2,position_option.y,'•')
+	drawString(position_cursor.x,position_cursor.y,'•')
 	drawString(position_option.x,position_option.y,MENU_OPTION.M_Normal)
 	drawString(position_option.x,position_option.y+1,MENU_OPTION.M_Infini)
 }
 
-initTerminal2DEngine()
-initKeyboard()
-drawMenuMode()
+function main() {
+	initTerminal2DEngine()
+	initKeyboard()
+	drawMenuMode()
+	onkey(applyDirection)
+}
+main()
