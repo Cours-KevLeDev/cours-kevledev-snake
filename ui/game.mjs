98af882e56e164d581cc8a2a6cd69ecbf67a3892
Kévin ʘ‿ʘ <stitchuuuu@icloud.com>
2026-01-29 12:44:00 +0100

    Affiche le menu au milieu

--- ui/game.mjs
+++ ui/game.mjs
@@ -1,10 +1,27 @@
-import { drawString } from "../engine/terminal-engine.mjs"
+import {
+	drawString,
+	getScreenWidth,
+	getScreenHeight
+} from "../engine/terminal-engine.mjs"
+
+/**
+ * Position du menu (centré au milieu de l'écran)
+ * @returns {{x: number, y: number}} Position du menu
+ */
+function getInfiniteModeMenuPos() {
+	return { 
+		x: Math.ceil((getScreenWidth()/2)-(INFINITE_MODE_OPTIONS[0].length/2)),
+		y: Math.ceil(getScreenHeight()/2)
+	}
+}
 
 /**
  * Affiche le menu
  */
 export function drawMenuMode() {
-	drawString(0, 0, INFINITE_MODE_OPTIONS[0])
-	drawString(0, 1,INFINITE_MODE_OPTIONS[1])
+	const pos = getInfiniteModeMenuPos()
+	// On pourrait faire une boucle
+	drawString(pos.x, pos.y, INFINITE_MODE_OPTIONS[0])
+	drawString(pos.x, pos.y+1 ,INFINITE_MODE_OPTIONS[1])
 }
 
